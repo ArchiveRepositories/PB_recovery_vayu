01b487ef845c672d230da5d70bcc452c35efacc9
johnmart19 <johnivan19999@gmail.com>
2021-10-23 11:57:35 +0300

    PB Test

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+DEVICE_PATH := device/xiaomi/vayu
+
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
 
 # Architecture
 TARGET_ARCH := arm64
